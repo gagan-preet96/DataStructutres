0458a258569d06a35541f4f2ef467c3f92b5d912
Gaganpreet Singh <gagan20683@gmail.com>
2020-07-15 00:45:06 +0530

    Sort an array in wave form

---
+++ src/array/WaveSorting.java
@@ -0,0 +1,48 @@
+package array;
+
+//Sort an array in wave form
+//An array ‘arr[0..n-1]’ is sorted in wave form if arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4] >= …..
+
+// we will traverse array only once , check that all the even index elements are greater than their adjacent odd index elements
+public class WaveSorting {
+
+	void swap(int arr[], int a ,int b)
+	{
+		int temp;
+		temp=arr[a];
+		arr[a]=arr[b];
+		arr[b]=temp;
+	}
+	
+	void waveSorting(int [] arr, int n)
+	{
+		for (int i=0;i<n ;i= i+2)
+		{
+			//if current element is smaller than prevoius
+			if(i>0 && arr[i-1] > arr[i])
+				swap(arr, i-1 ,i);
+			
+			//if current element is smaller than next
+			if(i<n-1 && arr[i] <arr[i+1])
+				swap(arr,i,i+1);
+		}
+	}
+	void printArray(int []arr,int n) 
+	{
+		for(int i=0;i<n;i++) 
+		{
+			System.out.println(arr[i] + " ");
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WaveSorting wss = new WaveSorting();
+		int [] arr = {10,90,49,2,1,5,23};
+		int n=arr.length;
+		
+		wss.waveSorting(arr, n);
+		wss.printArray(arr, n);
+	}
+
+}
